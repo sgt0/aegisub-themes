425cabbce01bffc82951625cbfe3fe03100edbee
sgt0 <140186177+sgt0@users.noreply.github.com>
2024-08-24 05:57:29 +0000

    deps: update chroma-js to v3.0.0

--- src/themes/utils.test.ts
+++ src/themes/utils.test.ts
@@ -4,10 +4,10 @@ import { meetContrastRatio } from './utils';
 test('meet contrast ratio', () => {
   const CASES = [
     /* eslint-disable sort-keys-fix/sort-keys-fix */
-    { fg: '#4C4F69', bg: '#DC8A78', expected: '#FFAF9A' /* #FCB0A1 */ },
+    { fg: '#4C4F69', bg: '#DC8A78', expected: '#FFAF9B' /* #FCB0A1 */ },
     { fg: '#E6E1CF', bg: '#59C2FF', expected: '#006AA0' /* #2D6A8D */ },
-    { fg: '#E6E1CF', bg: '#FFB454', expected: '#905700' /* #855C28 */ },
-    { fg: '#5C6773', bg: '#36A3D9', expected: '#94F2FF' }
+    { fg: '#E6E1CF', bg: '#FFB454', expected: '#915700' /* #855C28 */ },
+    { fg: '#5C6773', bg: '#36A3D9', expected: '#93F2FF' }
     /* eslint-enable sort-keys-fix/sort-keys-fix */
   ];
 
