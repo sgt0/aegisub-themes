b424a31d5b2f2dbffdc5f2b1792e3ed88a1e4832
sgt0 <140186177+sgt0@users.noreply.github.com>
2026-03-18 16:16:32 +0000

    deps: update dependency vite to v8

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import react from '@vitejs/plugin-react';
 import vike from 'vike/plugin';
 import type { UserConfig } from 'vite';
-import { cjsInterop } from 'vite-plugin-cjs-interop';
 
 const config: UserConfig = {
   base: '/aegisub-themes/',
-  plugins: [
-    cjsInterop({ dependencies: ['@mdi/react', 'chroma-js'] }),
-    react(),
-    vike()
-  ]
+  plugins: [react(), vike()]
 };
 
 export default config;
